ed3e2bcd347b13367d7e922a61b601aa2b6b72b4
mehakrajrana <mehakrajrana1@gmail.com>
2023-11-29 10:50:21 -0500

    v3

--- MyFSharpApp/Program.fs
+++ MyFSharpApp/Program.fs
@@ -1,55 +1,113 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 // TypeScript Example
 // Define the discriminated union genre
-type Genre = 
- |Horror
- |Drama
- |Thriller  
- |Comedy 
- |Fantasy
- |Sport;
+// Discriminated Union for Genre without 'kind'
+type Genre =
+    | Horror
+    | Drama
+    | Thriller
+    | Comedy
+    | Fantasy
+    | Sport
 
-// Define the Director record type
-type Director = {
-  name: string;
-  movies: number;
-};
+type Dir = {
+    Name: string
+    Movies: int
+}
 
-// Define the Movie record type
 type Movie = {
-  name: string;
-  runLength: number;
-  genre: Genre;
-  director: Director;
-  imdbRating: number;
-};
+    Name: string
+    RunLength: int
+    Genre: Genre
+    Director: Dir
+    IMDBRating: float
+}
 
-// Example data
-const directors: Director[] = [
-  { name: 'Director1', movies: 3 },
-  { name: 'Director2', movies: 5 },
-  // Add more directors as needed
-];
-
-const movies: Movie[] = [
-  { name: 'Movie1', runLength: 120, genre: 'Drama', director: directors[0], imdbRating: 8.5 },
-  { name: 'Movie2', runLength: 110, genre: 'Comedy', director: directors[1], imdbRating: 7.8 },
-  // Add more movies as needed
-];
-
-// Filter movies by genre (example: get all Drama movies)
-const dramaMovies = movies.filter((movie) => movie.genre === 'Drama');
-
-// Map directors to their total number of movies
-const directorMovieCounts = directors.map((director) => ({
-  directorName: director.name,
-  totalMovies: director.movies,
-}));
-
-// Display the results
-console.log('Drama Movies:', dramaMovies);
-console.log('Director Movie Counts:', directorMovieCounts);
+let hardpart runLength =
+    let hours = runLength / 60
+    let minutes = runLength % 60
+    sprintf "%dh %02dmin" hours minutes
 
 
+let coda = {
+    Name = "CODA"
+    RunLength = 111
+    Genre = Drama
+    Director = { Name = "Sian Heder"; Movies = 9 }
+    IMDBRating = 8.1
+}
+
+let belfast = {
+    Name = "Belfast"
+    RunLength = 98
+    Genre = Comedy
+    Director = { Name = "Kenneth Branagh"; Movies = 23 }
+    IMDBRating = 7.3
+}
+
+let dontLookUp = {
+    Name = "Don't Look Up"
+    RunLength = 138
+    Genre = Comedy
+    Director = { Name = "Adam McKay"; Movies = 27 }
+    IMDBRating = 7.2
+}
+
+let driveMyCar = {
+    Name = "Drive My Car"
+    RunLength = 179
+    Genre = Drama
+    Director = { Name = "Ryusuke Hamaguchi"; Movies = 16 }
+    IMDBRating = 7.6
+}
+
+let dune = {
+    Name = "Dune"
+    RunLength = 155
+    Genre = Fantasy
+    Director = { Name = "Denis Villeneuve"; Movies = 24 }
+    IMDBRating = 8.1
+}
+
+let kingRichard = {
+    Name = "King Richard"
+    RunLength = 144
+    Genre = Sport
+    Director = { Name = "Reinaldo Marcus Green"; Movies = 15 }
+    IMDBRating = 7.5
+}
+
+let licoricePizza = {
+    Name = "Licorice Pizza"
+    RunLength = 133
+    Genre = Comedy
+    Director = { Name = "Paul Thomas Anderson"; Movies = 49 }
+    IMDBRating = 7.4
+}
+
+let nightmareAlley = {
+    Name = "Nightmare Alley"
+    RunLength = 150
+    Genre = Thriller
+    Director = { Name = "Guillermo del Toro"; Movies = 22 }
+    IMDBRating = 7.1
+}
 
 
+let movies = [coda; belfast; dontLookUp; driveMyCar; dune; kingRichard; licoricePizza; nightmareAlley]
+
+let run = List.map (fun movie -> hardpart movie.RunLength) movies
+
+let oscarWinners = List.filter (fun movie -> movie.IMDBRating > 7.4) movies
+
+printfn "And the oscar goes to"
+oscarWinners |> List.iter (fun movie -> 
+    printfn "%s" movie.Name
+    printfn "rating: %.2f" movie.IMDBRating
+    printfn ""
+)
+
+printfn "Time:"
+movies |> List.iter (fun movie -> 
+    printfn "%s  %s" movie.Name (hardpart movie.RunLength)
+)
