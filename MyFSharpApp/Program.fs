324a565c37533f0f69dca950a61a868954cf28f9
mehakrajrana <mehakrajrana1@gmail.com>
2023-11-29 08:51:48 -0500

    v1

---
+++ MyFSharpApp/Program.fs
@@ -0,0 +1,10 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+// TypeScript Example
+// TypeScript Example
+type Genre = 'Horror' | 'Drama' | 'Thriller' | 'Comedy' | 'Fantasy' | 'Sport';
+
+// Usage
+const movieGenre: Genre = 'Thriller';
+const bookGenre: Genre = 'Fantasy';
+
+
